044154b77d1151946a1e9792920ac78da1cf034c
Muhammad Abbas J <104298022+MuhammadAbbas010@users.noreply.github.com>
2024-04-30 17:54:40 +0800

    Add files via upload

---
+++ Past Paper Programs/October 2023, Question 3/Module2.vb
@@ -0,0 +1,23 @@
+﻿Module Module2
+    Sub Main()
+        Dim data As String
+        Dim id(49) As Integer
+        Dim index As Integer = 0
+        FileOpen(1, "C:\Users\0372461\Desktop\Queue.txt", OpenMode.Input)
+        Do
+            data = LineInput(1)
+            Console.WriteLine(data)
+            id(index) = data
+            index += 1
+
+        Loop Until EOF(1)
+        FileClose(1)
+
+        Console.WriteLine("Starting Array output: ")
+        For x = 1 To index
+            Console.WriteLine(id(x))
+        Next
+
+        Console.ReadKey()
+    End Sub
+End Module
