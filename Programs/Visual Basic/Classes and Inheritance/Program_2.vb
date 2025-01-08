730edcedb669f085be9aa703c6655c1d3f233caa
Muhammad Abbas J. <104298022+MuhammadAbbas010@users.noreply.github.com>
2025-01-08 23:16:26 +0800

    Add files via upload

---
+++ Programs/Visual Basic/Classes and Inheritance/Program_2.vb
@@ -0,0 +1,16 @@
+Imports System
+
+Module Program
+    Class Vehicle
+        Public Vehicle As String
+    End Class
+
+    Sub Main(args As String())
+        Dim v1, v2, v3 As New Vehicle
+        v1.Vehicle = "Lorry"
+        v2.Vehicle = "Sedan"
+        v3.Vehicle = "bus"
+
+        Console.ReadKey()
+    End Sub
+End Module
