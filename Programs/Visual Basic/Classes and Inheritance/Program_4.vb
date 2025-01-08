d8de745c8f0556ee7a26d1f7a1fc897ea800b54d
Muhammad Abbas J. <104298022+MuhammadAbbas010@users.noreply.github.com>
2025-01-08 23:17:02 +0800

    Add files via upload

---
+++ Programs/Visual Basic/Classes and Inheritance/Program_4.vb
@@ -0,0 +1,18 @@
+Imports System
+
+Module Program
+    Class Worker
+        Public Name As String
+        Public Gender As Boolean
+        Public Salary As Integer
+        Public Origin As String
+    End Class
+    Sub Main(args As String())
+        Dim nm As New Worker
+        Dim gender As New Worker
+        Dim sal As New Worker
+        Dim org As New Worker
+
+        Console.ReadKey()
+    End Sub
+End Module
