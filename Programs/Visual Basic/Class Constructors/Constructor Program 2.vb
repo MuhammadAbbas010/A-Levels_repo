33a17b2506f88dcf9add65439d6151fb4cbfa3c4
Muhammad Abbas J. <104298022+MuhammadAbbas010@users.noreply.github.com>
2025-01-15 20:17:10 +0800

    Add files via upload

--- Programs/Visual Basic/Class Constructors/Constructor Program 2.vb
+++ Programs/Visual Basic/Class Constructors/Constructor Program 2.vb
@@ -1,21 +1,51 @@
 Imports System
 
-
-Class Person
-        Private_Name As String
-
-        Public Sub New(ByVal name As String)
-        MyClass._name = name
-
-        Console.WriteLine(MyClass._Name)
-            Console.WriteLine(name)
-            Console.WriteLine(_name)
-        End Sub
+Module Program
+    Class Shape
+        Public Overridable Function CalculateArea() As Double
+            Return 0
+        End Function
     End Class
 
-    Module Program
-        Sub Main(args As String())
-        Dim p As Person = New Person("Sam")
+    Class Circle
+        Inherits Shape
+
+        Public Property Radius As Double
+
+        Public Sub New(ByVal rad As Double)
+            Radius = rad
+        End Sub
+
+        Public Overrides Function CalculateArea() As Double
+            Return (3.14) * Math.Pow(Radius, 2)
+        End Function
+    End Class
+
+    Class Rectangle
+        Inherits Shape
+        Public Property Height As Double
+        Public Property width As Double
+
+        Public Sub New(ByVal h As Double, ByVal w As Double)
+            Height = Height
+            width = w
+        End Sub
+
+        Public Overrides Function CalculateArea() As Double
+            Return Height * width
+        End Function
+    End Class
+
+    Sub Main(args As String())
+        Dim shape As Shape = New Shape()
+        Dim circle As Shape = New Circle(3.0)
+        Dim rectangle As Shape = New Rectangle(3.0, 4.0)
+
+        Console.WriteLine("The area of shape is " & shape.CalculateArea())
+        Console.WriteLine("The area of circle is " & circle.CalculateArea())
+        Console.WriteLine("The area of rectangle is " & rectangle.CalculateArea())
+
         Console.ReadKey()
+
     End Sub
-    End Module
+End Module
